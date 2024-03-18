2fe3d1e7c2007ece39d84d25f416f7cddb35ebbf
JakeLawhorne <jake.lawhorne@gmail.com>
2024-03-18 18:19:13 -0400

    done

--- src/main/java/com/github/curriculeon/GenericInventory.java
+++ src/main/java/com/github/curriculeon/GenericInventory.java
@@ -1,31 +1,51 @@
 package com.github.curriculeon;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by leon on 12/16/2019.
  */
-public class GenericInventory<SomeType> implements InventoryInterface<SomeType> {
+public class GenericInventory<SomeType> implements InventoryInterface<SomeType>, Iterable<SomeType> {
+    private List<SomeType> itemList = new ArrayList<>();
+
     @Override
     public void add(SomeType someObject) {
-
+        itemList.add(someObject);
     }
 
     @Override
     public Boolean contains(SomeType someObject) {
-        return null;
+        return itemList.contains(someObject);
     }
 
     @Override
     public SomeType get(int indexOfElement) {
-        return null;
+        return itemList.get(indexOfElement);
     }
 
     @Override
     public SomeType remove(int indexOfElement) {
-        return null;
+        SomeType item = itemList.get(indexOfElement);
+        itemList.remove(item);
+        return item;
     }
 
     @Override
     public SomeType[] toArray(SomeType[] objectsToBeAdded) {
-        return null;
+        return itemList.stream().toArray((ho)-> objectsToBeAdded);
+    }
+    @Override
+    public Iterator<SomeType> iterator() {
+        return itemList.iterator();
+    }
+    public List<SomeType> getItemList() {
+        return itemList;
+    }
+
+    public GenericInventory() {
+
     }
 }
+
